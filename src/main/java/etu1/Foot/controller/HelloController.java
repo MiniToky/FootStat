56ef2fe45c7574f20ab6308c0433e2d1da03dba1
Emmanuel Rafidinjanahary <97870569+MiniToky@users.noreply.github.com>
2023-12-02 19:18:53 +0300

    :)

--- src/main/java/etu1/Foot/controller/HelloController.java
+++ src/main/java/etu1/Foot/controller/HelloController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
     @RequestMapping
     public String hello(){
-        return "Hello Ken!";
+        return "Hello Bokona !";
     }
 }
